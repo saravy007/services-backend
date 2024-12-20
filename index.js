3940bed6a6f14a7e8a6798b459bf3c4d87953bea
saravy meas <saravy.meas.007@gmail.com>
2024-12-20 21:34:31 +0700

    add cors

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ const userProfileRouter = require("./routes/user-profile.js");
 const fileAttachRouter = require("./routes/file-attach.js");
 const key = fs.readFileSync("localhost-key.pem", "utf-8");
 const cert = fs.readFileSync("localhost.pem", "utf-8");
+const cors = require("cors");
+
+app.use(cors());
 
 dbConnect().catch((err) => {
   console.log(err);
@@ -31,8 +34,16 @@ app.use("/auth", authRouter);
 app.use("/qr-scan", qrRouter);
 app.use("/users", passport.authenticate("jwt", { session: false }), userRouter);
 app.use("/form", passport.authenticate("jwt", { session: false }), appRouter);
-app.use("/user-profile", passport.authenticate("jwt", { session: false }), userProfileRouter);
-app.use("/file-attach", passport.authenticate("jwt", { session: false }), fileAttachRouter);
+app.use(
+  "/user-profile",
+  passport.authenticate("jwt", { session: false }),
+  userProfileRouter
+);
+app.use(
+  "/file-attach",
+  passport.authenticate("jwt", { session: false }),
+  fileAttachRouter
+);
 
 // Basic error handling middleware
 app.use(errorHandle);
